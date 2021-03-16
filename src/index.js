0b791627e597ce3ffc494e3b4b76fb4b974b70b0
Vlad Deputat <deputatwladek@gmail.com>
2021-03-16 23:15:19 +0200

    done

--- src/index.js
+++ src/index.js
@@ -5,25 +5,30 @@ const hoursRef = document.querySelector('[data-value = "hours"]');
 const minsRef = document.querySelector('[data-value = "mins"]');
 const secsRef = document.querySelector('[data-value = "secs"]');
 
+const currentTime = targetDate => {
+  let time = targetDate - new Date();
+  daysRef.textContent = String(
+    Math.floor(time / (1000 * 60 * 60 * 24)),
+  ).padStart(2, '0');
+  hoursRef.textContent = String(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  ).padStart(2, '0');
+  minsRef.textContent = String(
+    Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+  ).padStart(2, '0');
+  secsRef.textContent = String(
+    Math.floor((time % (1000 * 60)) / 1000),
+  ).padStart(2, '0');
+};
+
 class CountdownTimer {
   constructor({ targetDate }) {
     setInterval(() => {
-      let time = targetDate - new Date();
-      daysRef.textContent = String(
-        Math.floor(time / (1000 * 60 * 60 * 24)),
-      ).padStart(2, '0');
-      hoursRef.textContent = String(
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      ).padStart(2, '0');
-      minsRef.textContent = String(
-        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
-      ).padStart(2, '0');
-      secsRef.textContent = String(
-        Math.floor((time % (1000 * 60)) / 1000),
-      ).padStart(2, '0');
+      currentTime(targetDate);
     }, 1000);
   }
 }
+currentTime(new Date('Sep 28, 2021'));
 
 new CountdownTimer({
   selector: '#timer-1',
